59b55531fd0c66cece8c1d1bde53487115f84d25
Ramon Del Valle <ramon.delvalle@icloud.com>
2016-07-01 20:31:45 -0500

    Create Data Access Service

---
+++ src/app/common/data-access-service.js
@@ -0,0 +1,24 @@
+var app;
+(function (app) {
+    var common;
+    (function (common) {
+        'use strict';
+        /**
+         * DataAccessService implements IDataAccessService
+         */
+        var DataAccessService = (function () {
+            function DataAccessService($resource) {
+                this.$resource = $resource;
+            }
+            DataAccessService.prototype.getContactResource = function () {
+                return this.$resource('api/contacts/:id');
+            };
+            DataAccessService.$inject = ['$resource'];
+            return DataAccessService;
+        }());
+        common.DataAccessService = DataAccessService;
+        angular
+            .module('common.services')
+            .service('dataAccessService', DataAccessService);
+    })(common = app.common || (app.common = {}));
+})(app || (app = {}));
---
+++ src/app/common/data-access-service.ts
@@ -0,0 +1,32 @@
+module app.common {
+    'use strict';
+    interface IDataAccessService {
+        getContactResource(): ng.resource.IResourceClass<IContactResource>
+    }
+
+    interface IContactResource 
+        extends ng.resource.IResource<app.domain.IContact> {
+
+    }
+
+    /**
+     * DataAccessService implements IDataAccessService
+     */
+    export class DataAccessService 
+        implements IDataAccessService {
+
+        static $inject = ['$resource'];
+        constructor(private $resource: ng.resource.IResourceService) {
+            
+        }
+
+        getContactResource(): ng.resource.IResourceClass<IContactResource> {
+            return this.$resource('api/contacts/:id');
+        }
+    }
+
+    angular
+        .module('common.services')
+        .service('dataAccessService',
+            DataAccessService);
+}
