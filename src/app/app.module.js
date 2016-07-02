1389f04e021461211d5ffd7215c664021357bd08
Ramon Del Valle <ramon.delvalle@icloud.com>
2016-07-01 22:02:47 -0500

    Add Routing

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,8 +1,19 @@
 var app;
 (function (app) {
     'use strict';
-    angular.module('contactManagement', [
+    angular
+        .module('contactManagement', ['ngRoute',
         'common.services',
         'contactResourceMock'
-    ]);
+    ])
+        .config(routeConfig);
+    routeConfig.$inject = ['$routeProvider'];
+    function routeConfig($routeProvider) {
+        $routeProvider
+            .when('/contactList', {
+            templateUrl: './app/contacts/contact-list-view.html',
+            controller: 'ContactListCtrl as vm'
+        })
+            .otherwise('/contactList');
+    }
 })(app || (app = {}));
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,20 @@
 module app {
     'use strict';
-    angular.module('contactManagement', [
-        'common.services',
-        'contactResourceMock'
-    ]);
+    angular
+        .module('contactManagement', ['ngRoute',
+                                    'common.services',
+                                    'contactResourceMock'
+                                        ])
+        .config(routeConfig);
+
+    routeConfig.$inject = ['$routeProvider'];
+    function routeConfig($routeProvider: ng.route.IRouteProvider):void {
+        $routeProvider
+            .when('/contactList',
+                {
+                    templateUrl: './app/contacts/contact-list-view.html',
+                    controller: 'ContactListCtrl as vm'    
+                })
+            .otherwise('/contactList');
+    }
 }
